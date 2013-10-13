29b588403c5d961aab0d4336466b063875820b8c
Knight Fu <knight.w.fu@gmail.com>
2013-10-12 22:27:24 -0400

    first commit, pushing rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pachybank::Application.config.secret_key_base = '62a3baf2fada5d5f683fb3f7dccfc31a9e82e5c643748d2e287a5e38faef794e94e7e7aeeded6ea9a44bb0c7c384620aa40ec786988ebf392ce71773387773ae'
